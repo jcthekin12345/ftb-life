5f67766618c50a7cc297e415b4f5baf182a007c5
JctheNinth753 <93554396+jcthekin12345@users.noreply.github.com>
2024-08-24 13:28:18 +0200

    Add: created player generator nim file

--- prototypes/playerGen.py
+++ prototypes/playerGen.py
@@ -1,2 +1,12 @@
 
-class Pro
+def player(*args):
+    """A player generation prototype"""
+    playerInfo = [i for i in args]
+    return playerInfo
+
+def player_gen():
+    player()
+
+
+if __name__ == "__main__":
+    print(player("name", 5, "10"))
