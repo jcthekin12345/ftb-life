c8f6b18f372e9e65c4fbe07bd808e3969fd87477
JctheNinth753 <93554396+jcthekin12345@users.noreply.github.com>
2024-08-25 16:31:02 +0200

    Add: Comments for better readability.

--- prototypes/mainMenu.py
+++ prototypes/mainMenu.py
@@ -1,11 +1,32 @@
 
 from playerMenu import *
+from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Container, Vertical
-from textual.widgets import Button, Static
+from textual.widgets import Button, Label, Static
 
+
+class NewGameMenu(Screen[bool]):
+    """New Game menu prompt"""
+    def __init__(self, question: str):
+        self.question = question
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Label(self.question)
+        yield Button("Yes", id="yes", variant="success")
+        yield Button("No", id="no")
+
+    @on(Button.Pressed, "#yes")
+    def handle_yes(self) -> None:
+        self.dismiss(True)
+
+    @on(Button.Pressed, "#no")
+    def handle_no(self) -> None:
+        self.dismiss(False)
 
 class MainMenu(App[str]):
+    """Main menu class"""
 
     CSS_PATH = "mainMenu.tcss"
 
@@ -22,13 +43,13 @@ class MainMenu(App[str]):
             id="mainMenu",
         )
     def on_button_pressed(self, event: Button.Pressed) -> None:
+        """on_button_pressed method for check events"""
         if event.button.id == "exit":
             self.exit()
         elif event.button.id == "new-game":
-            self.app.push_screen("ftbPlayerCreation")
-
+            self.app.push_screen("createPrompt")
+#Main function
 def main():
-
     app = MainMenu()
     reply = app.run()
     print(reply)
