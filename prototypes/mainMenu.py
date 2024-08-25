0cb25fb3da7c7816c9c0fdb1864b23080024bc3e
JctheNinth753 <93554396+jcthekin12345@users.noreply.github.com>
2024-08-25 14:24:53 +0200

    Rearrange: player function code

--- prototypes/mainMenu.py
+++ prototypes/mainMenu.py
@@ -1,4 +1,5 @@
 
+from playerMenu import *
 from textual.app import App, ComposeResult
 from textual.containers import Container, Vertical
 from textual.widgets import Button, Static
@@ -7,7 +8,6 @@ from textual.widgets import Button, Static
 class MainMenu(App[str]):
 
     CSS_PATH = "mainMenu.tcss"
-
 
     def compose(self) -> ComposeResult:
         yield Container(
@@ -27,24 +27,9 @@ class MainMenu(App[str]):
         elif event.button.id == "new-game":
             self.app.push_screen("ftbPlayerCreation")
 
-
-
-class Player:
-    def __init__(self, **kwargs):
-        self.name  = kwargs.get("name")
-        self.age = kwargs.get("age")
-        self.position = kwargs.get("position")
-
-    def __str__(self):
-        return f"name: {self.name} age: {self.age} positions: {self.position}"
-
-    def createPlayer(self):
-        print("Create your player.")
-
 def main():
 
-    print(Player(name="Joaquin Coetzee", age=18, position=("CAM", "AMC", "AML", "AMR")))
-    app = FtbMainMenu()
+    app = MainMenu()
     reply = app.run()
     print(reply)
 
--- prototypes/playerMenu.py
+++ prototypes/playerMenu.py
@@ -28,3 +28,15 @@ class CreatePrompt(Screen):
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "exit":
             self.app.push_screen(self.ftbMain)
+
+class Player:
+    def __init__(self, **kwargs):
+        self.name  = kwargs.get("name")
+        self.age = kwargs.get("age")
+        self.position = kwargs.get("position")
+
+    def __str__(self):
+        return f"name: {self.name} age: {self.age} positions: {self.position}"
+
+    def createPlayer(self):
+        print("Create your player.")
