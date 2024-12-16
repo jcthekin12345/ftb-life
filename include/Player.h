13af2eb07fec5146fa3debef098ab6f0a7ebaf01
Jctheking123456 <93554396+jcthekin12345@users.noreply.github.com>
2024-12-16 11:23:10 +0200

    Add roles and skills that come with those roles

--- include/Player.h
+++ include/Player.h
@@ -7,16 +7,27 @@
 #include <string>
 #include <unordered_map>
 #include "../include/Skill.h"
+#include <vector>
 
 class Player {
 private:
   std::string _name;
-  std::string _surname;
+  std::string _lastName;
+  std::string _role;
   int _age;
+  std::string country;
   std::unordered_map<std::string, Skill> skills;
   // Add additional attributes as needed
 
+  static const std::unordered_map<std::string, std::vector<std::string>> roleSkills;
+
 public:
+  Player(const std::string& playerName
+    , const std::string& playerLastName
+    , const std::string& playerRole);
+  void incrementSkill(const std::string& skillName, int& value);
+  void decrementSkill(const std::string& skillName, int& value);
+  void displaySkills() const;
 
 };
 
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,5 +1,24 @@
-//
-// Created by jctheking on 12/16/24.
+// // Created by jctheking on 12/16/24.
 //
 
-#include "Player.h"
+#include "../include/Player.h"
+#include "../include/Skill.h"
+
+Player::Player(const std::string& playerName
+  , const std::string& playerLastName
+  , const std::string& playerRole)
+  : _name(playerName), _lastName(playerLastName), _role(playerRole){
+
+  // Initialize skills based on the player's role
+  if (roleSkills.contains(_role) ) {
+    for (const auto& skillName : roleSkills.at(_role)) {
+      skills[skillName] = Skill(skillName);
+    }
+  }
+}
+
+const std::unordered_map<std::string, std::vector<std::string>> Player::roleSkills = {
+  {"Striker", {"Shooting", "Positioning", "Dribbling", "Pace"}},
+  {"Defender", {"Tackling", "Marking", "Heading", "Strength"}},
+  {"Midfielder", {"Passing", "Vision", "Dribbling", "Stamina"}}
+};
