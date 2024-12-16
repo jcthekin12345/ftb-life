0cc71b74f0544ce4de30e0510954c78a849e38f4
Jctheking123456 <93554396+jcthekin12345@users.noreply.github.com>
2024-12-16 09:25:11 +0200

    Reorganized files and added skills to player

--- include/Player.h
+++ include/Player.h
@@ -5,12 +5,19 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <string>
+#include <unordered_map>
+#include "../include/Skill.h"
 
 class Player {
 private:
   std::string _name;
   std::string _surname;
   int _age;
+  std::unordered_map<std::string, Skill> skills;
+  // Add additional attributes as needed
+
+public:
+
 };
 
 
