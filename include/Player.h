8e1489bd9ee3424ca764e53fdf1b602e056b8148
Jctheking123456 <93554396+jcthekin12345@users.noreply.github.com>
2024-12-16 09:22:12 +0200

    Add player file

---
+++ include/Player.h
@@ -0,0 +1,17 @@
+//
+// Created by jctheking on 12/16/24.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+
+class Player {
+private:
+  std::string _name;
+  std::string _surname;
+  int _age;
+};
+
+
+#endif //PLAYER_H
